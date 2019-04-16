53c573515ce3e65097c1b102de5cfef4e34bfd95
Henry Hirsch <henry@w3-net.de>
2019-04-16 22:36:40 +0200

    Add very basic gamepad controls

--- source/Graphics.cpp
+++ source/Graphics.cpp
@@ -1,23 +1,60 @@
 #include <SFML/Graphics.hpp>
+#include <string>
+
+sf::VideoMode getDesktopVideoMode(sf::VideoMode desktop) {
+
+  return sf::VideoMode(desktop.width, desktop.height, desktop.bitsPerPixel);
+}
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Graphics Demo");
-    sf::CircleShape shape(50.f);
-    shape.setFillColor(sf::Color(100, 250, 50));
-    shape.setPosition(400, 300);
+  sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+  sf::RenderWindow window(getDesktopVideoMode(desktop), "Graphics Demo");
+  sf::Text text;
+  sf::Font font;
+  font.loadFromFile("/usr/share/fonts/TTF/impact.ttf");
+  text.setFont(font);
+  text.setCharacterSize(24);
+  text.setPosition(10, 10);
+  sf::CircleShape shape(10.f);
+  shape.setFillColor(sf::Color(100, 250, 50));
 
+  float x = desktop.width/2;
+  float y = desktop.height/2;
+    bool connected;
     while (window.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        window.clear(sf::Color::Black);
-        window.draw(shape);
-        window.display();
+      connected = sf::Joystick::isConnected(0);
+      if (!connected) {
+        exit(1);
+      }
+      sf::Event event;
+      while (window.pollEvent(event)) {
+        if (event.type == sf::Event::Closed)
+          window.close();
+      }
+      text.setString(std::to_string(x) + "/" + std::to_string(y));
+      shape.setPosition(x, y);
+      if (sf::Joystick::getAxisPosition(0, sf::Joystick::Y) > 50) {
+        y += 0.1;
+      }
+
+      if (sf::Joystick::getAxisPosition(0, sf::Joystick::Y) < -50) {
+        y -= 0.1;
+      }
+
+      if (sf::Joystick::getAxisPosition(0, sf::Joystick::X) > 50) {
+        x += 0.1;
+      }
+
+      if (sf::Joystick::getAxisPosition(0, sf::Joystick::X) < -50) {
+        x -= 0.1;
+      }
+
+      window.clear(sf::Color::Black);
+      window.draw(text);
+      window.draw(shape);
+      window.display();
     }
 
     return 0;
